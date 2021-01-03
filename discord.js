9eb6cb600f5899992e06771bdc811f6cb7da98e8
sherwin wyco <sherwin.wyco20@gmail.com>
2021-01-03 05:44:47 -0500

    changed prefix

--- discord.js
+++ discord.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("Type !help");
+  client.user.setActivity("Type -help");
 });
 
 const prefix = process.env.DISCORD_PREFIX;
