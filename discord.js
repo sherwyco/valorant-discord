b8f536c6f23a9141b730da70688a838a97246079
sherwin wyco <sherwin.wyco20@gmail.com>
2021-01-03 05:44:20 -0500

    fixed error

--- discord.js
+++ discord.js
@@ -5,6 +5,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("Type !help");
 });
 
 const prefix = process.env.DISCORD_PREFIX;
@@ -53,5 +54,5 @@ client.on("message", msg => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
-client.setActivity("Type -help");
+
 module.exports = client;
